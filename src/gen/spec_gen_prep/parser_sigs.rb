b801fda9bc8c7422c9ad61c67d931f6ca7faf847
calicoday <40372440+calicoday@users.noreply.github.com>
2022-08-09 13:47:26 -0400

    More spec_gen. Added wrapping/unwrapping FFI::MemoryPointer args.

--- src/gen/spec_gen_prep/parser_sigs.rb
+++ src/gen/spec_gen_prep/parser_sigs.rb
@@ -19,7 +19,15 @@ class ParserSigs
 			TreeSitterFFI::Range.new, 1',
 			['# arg_1 is array of Range, arg_2 is array len.']
 			]
-		when :ts_parser_included_ranges then [nil,
+		# make these spec_gen test cases:
+# 		when :ts_parser_set_included_ranges then [['@pars', 
+# 			'TreeSitterFFI::Range.new', '1', :uint32_p],
+# 			['# arg_1 is array of Range, arg_2 is array len.']
+# 			]
+# 		when :ts_parser_included_ranges then [['@pars', [:uint32_p], :quux],
+# 		  ['# ret is array of Range, arg_1 is pointer to array len.']
+# 		  ]
+		when :ts_parser_included_ranges then [['@pars', :uint32_p],
 		  ['# ret is array of Range, arg_1 is pointer to array len.']
 		  ]
 # 		when :ts_parser_parse then 
